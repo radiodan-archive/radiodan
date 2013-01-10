bf3452a27c287b1601eff3512589b72d888eb2aa
Dan Nuttall <pixelblend@gmail.com>
2013-01-10 23:22:06 +0000

    Basic playback software installed, alsa ready to play

--- manifests/radiodan.pp
+++ manifests/radiodan.pp
@@ -1,3 +1,8 @@
+exec { "apt-update":
+    command => "/usr/bin/apt-get update"
+  }
+
+Exec["apt-update"] -> Package <| |>
 Package { ensure => "installed" }
 
 package { "ruby1.9.3": } 
@@ -5,19 +10,25 @@ package { "ruby1.9.3": }
 package { 'bundler':
   ensure   => 'installed',
   provider => 'gem',
+  require => Package['ruby1.9.3'],
 }
 
+package { "vim": } 
 package { "screen": } 
+
 package { "mpd": } 
 package { "mpc": } 
-package { "vim": } 
+package { "ncmpcpp": } 
+package { "alsa-utils": } 
 
 service { "mpd":
   ensure => "running",
+  require => Package['mpd'],
 }
 
 exec { "umute":
   command => "amixer set -c 0 Master 70 unmute && amixer set -c 0 PCM 70 unmute",
   path    => "/usr/bin/",
+  require => Package['alsa-utils'],
 }
 
