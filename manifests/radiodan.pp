06370aa1bf755681a85898c4ee8de92dedf16987
Dan Nuttall <pixelblend@gmail.com>
2013-01-13 22:24:14 +0000

    Add MPD setup, exec BBC Radio Playlists on provision

--- manifests/radiodan.pp
+++ manifests/radiodan.pp
@@ -20,15 +20,48 @@ package { "mpd": }
 package { "mpc": } 
 package { "ncmpcpp": } 
 package { "alsa-utils": } 
+package { "curl": } 
 
 service { "mpd":
   ensure => "running",
   require => Package['mpd'],
 }
 
+exec { "radio-playlists":
+  command => "/vagrant/bin/radio-playlists.sh",
+  require => Package['curl'],
+}
+
 exec { "umute":
   command => "amixer set -c 0 Master 70 unmute && amixer set -c 0 PCM 70 unmute",
   path    => "/usr/bin/",
   require => Package['alsa-utils'],
+}
+
+file { '/etc/mpd.conf':
+  ensure  => present,
+  require => Package['mpd'],
+  notify => Service['mpd'],
+  content => '
+    music_directory    "/vagrant/music"
+    playlist_directory "/vagrant/playlists"
+    db_file            "/var/lib/mpd/tag_cache"
+    log_file           "/var/log/mpd/mpd.log"
+    pid_file           "/var/run/mpd/pid"
+    user               "mpd"
+    bind_to_address    "localhost"
+    port               "6600"
+    auto_update        "yes"
+
+    audio_output {
+            type            "alsa"
+            name            "My ALSA Device"
+            device          "hw:0,0"
+            format          "44100:16:2"    
+            mixer_device    "default"
+            mixer_control   "PCM"
+            mixer_index     "0"
+    }
+  ',
 }
 
