918ac5d42a392e1d897c2fd5c9a7cc6d541597d7
Dan Nuttall <pixelblend@gmail.com>
2013-01-13 17:43:40 +0000

    Add playlist generation for BBC Radio

---
+++ bin/radio-playlists.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+playlists=$(dirname "$0")/../playlists
+
+for RADIO in 1 2 3 4 4lw 4x 5l 5lsp 6; do
+  curl -s http://www.bbc.co.uk/radio/listen/live/r${RADIO}_aaclca.pls | grep File1 | cut -d'=' -f2- > $playlists/bbc_radio_${RADIO}.m3u
+done
+
