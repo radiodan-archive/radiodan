89d7f00672bb61b3106f199885a50bf017455064
Dan Nuttall <pixelblend@gmail.com>
2013-04-20 19:01:06 +0100

    Memoize events

--- lib/radio/event_binding.rb
+++ lib/radio/event_binding.rb
@@ -1,18 +1,16 @@
 class Radio  
 module EventBinding
   def register_event(event, &blk)
-    @events ||= Hash.new{ |h, k| h[k] = [] }
-    
     logger.info "Registered event #{event}"
     event = event.to_sym
-    @events[event] << blk
+    events[event] << blk
     
     true
   end
   
   def trigger_event(event, *data)
     event = event.to_sym
-    event_bindings = @events[event]
+    event_bindings = events[event]
     
     unless event_bindings
       logger.error "Event #{event} triggered but not found" 
@@ -21,6 +19,11 @@ module EventBinding
     event_bindings.each do |blk|
       blk.call(data)
     end
+  end
+  
+  private
+  def events
+    @events ||= Hash.new{ |h, k| h[k] = [] }
   end
 end
 end
