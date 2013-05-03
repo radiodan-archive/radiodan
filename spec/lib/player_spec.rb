8aad2249a79a457839efb70de1f9bfce8fe10c8c
Dan Nuttall <pixelblend@gmail.com>
2013-05-03 17:33:19 +0100

    Add test harness

---
+++ spec/lib/player_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'player'
+
+describe Radiodan::Player do
+  it 'exists' do
+   #
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rspec/autorun'
+$: << File.expand_path('../../lib/radiodan', __FILE__)
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
