cf80b0c53d47aae75db518f64be39790c54d7a3c
Dan Nuttall <pixelblend@gmail.com>
2013-08-30 10:59:52 +0100

    Periodic timer must have a value greater than 0

--- lib/radiodan/em_additions.rb
+++ lib/radiodan/em_additions.rb
@@ -11,14 +11,16 @@ module EventMachine::Synchrony
   def self.now_and_every(period, &blk)
     seconds = case
       when period.respond_to?(:to_f)
-        period.to_f
+        period
       when period.include?(:hours)
         period[:hours]*60*60
       when period.include?(:minutes)
         period[:minutes]*60
       else
         period[:seconds]
-    end
+    end.to_f
+    
+    raise "Period must be higher than 0" if period == 0.0
     
     next_tick do
       yield
